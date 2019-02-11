5e32ddd49252bc3b9f7f0237685209f5024b21aa
Vano <hervelee99@gmail.com>
2019-02-11 14:58:59 +0100

    dev

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   
   def show
   	@user = current_user
-  	
-  end
-
-  def update
-  
   	@favdish = Dish.find(params[:format])
   	favdish_params = params.require(:users).permit(:favdishes)
   	@favdish.update(favdish_params)
   	redirect_to profil_path
+    
+  end
+
+  def update
+  
   end
 
 end
